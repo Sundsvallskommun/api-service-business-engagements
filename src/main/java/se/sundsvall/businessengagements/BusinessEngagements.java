60c7a1726b4ee3e1b2c49a2f0a5302c43c1f86eb
Martin Hansson <martinhansson80@gmail.com>
2024-06-07 11:17:32 +0200

    uf-8632: Replaced cache config classes with properties.

--- src/main/java/se/sundsvall/businessengagements/BusinessEngagements.java
+++ src/main/java/se/sundsvall/businessengagements/BusinessEngagements.java
@@ -3,6 +3,7 @@ package se.sundsvall.businessengagements;
 import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import se.sundsvall.dept44.ServiceApplication;
@@ -10,6 +11,7 @@ import se.sundsvall.dept44.ServiceApplication;
 @ServiceApplication
 @ConfigurationPropertiesScan("se.sundsvall.businessengagements")
 @EnableFeignClients
+@EnableCaching
 public class BusinessEngagements {
 	public static void main(String... args) {
 		run(BusinessEngagements.class, args);
